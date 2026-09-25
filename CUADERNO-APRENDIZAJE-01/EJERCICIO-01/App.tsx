de17c6c11f5a0785f4dd5257a87ac7b93c3d81b9
EnmaCP <a29316@svalero.com>
2026-09-25 08:56:25 +0200

    ejercicio 1

---
+++ CUADERNO-APRENDIZAJE-01/EJERCICIO-01/App.tsx
@@ -0,0 +1,29 @@
+import { StatusBar } from 'expo-status-bar';
+import { StyleSheet, Text, View } from 'react-native';
+
+export default function App() {
+  return (
+    <View style={styles.container}>
+      <Text style={styles.title}>React Native</Text>
+      <Text style={styles.subtitle}>Mi primera pantalla</Text>
+    </View>
+  );
+}
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    justifyContent: 'center',
+    alignItems: 'center',
+    backgroundColor: '#f1f5f9',
+  },
+  title: {
+    fontSize: 30,
+    fontWeight: 'bold',
+  },
+  subtitle: {
+    marginTop: 8,
+    fontSize: 16,
+    color: '#64748b',
+  },
+});
